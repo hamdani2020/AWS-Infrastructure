acf13d470284cc5994933252c5a9c3afcab414ea
hamdani2020 <hamdanialhassangandi@gmail.com>
2025-04-14 11:31:05 +0000

    removed default in providers

--- variables.tf
+++ variables.tf
@@ -2,9 +2,9 @@ variable "region" {
   default = "eu-west-1"
 }
 
-variable "profile" {
-  default = "default"
-}
+# variable "profile" {
+#   default = "default"
+# }
 
 variable "vpc_cidr" {
   default     = "10.0.0.0/16"
